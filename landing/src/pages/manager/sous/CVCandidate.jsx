c820cd965dda1989f901fd4f6dabd7739bb8cd78
mertinaclaudietoto <mertinaclaudietoto@gmail.com>
2026-01-05 08:46:51 +0100

    correction secrets secrets.ANSIBLE_PASSWORD

--- landing/src/pages/manager/sous/CVCandidate.jsx
+++ landing/src/pages/manager/sous/CVCandidate.jsx
@@ -5,7 +5,7 @@ import { Sidebar } from "../../../components";
 import { useParams } from "react-router-dom";
 import { url_recrutement, url_recrutement_image } from "../../../data/data";
 import { getData } from "../../../function/Axios";
-
+// modifi
 export default function CVCandidate(){
     const [experience ,setExperience]=useState('1');
     const { id } = useParams();
