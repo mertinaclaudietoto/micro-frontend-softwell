37276c9981296f899ca18c135bb3361cef19867b
mertinaclaudietoto <mertinaclaudietoto@gmail.com>
2026-01-05 08:05:22 +0100

    resolve wish not found

--- landing/src/pages/manager/sous/CVCandidate.jsx
+++ landing/src/pages/manager/sous/CVCandidate.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { infocandidate } from "../../../data/candidate";
+// import { infocandidate } from "../../../data/candidate";
 import { getAge,dateToLetters, diffDate } from "../../../function/Date";
 import { Sidebar } from "../../../components";
 import { useParams } from "react-router-dom";
--- landing/src/pages/training/wish.jsx
+++ landing/src/pages/training/wish.jsx
@@ -6,7 +6,7 @@ import { CardAddTraining, Filter,Sidebar,CardWish } from "../../components";
 import { url, textbackground } from "../../data/data";
 import { useState } from "react";
 import Select from "../../function/selectSimple";
-export default function Trainer(){
+export default function Wish(){
     // TODO:delete and update
     const acces = sessionStorage.getItem("access");
     const accesObj = JSON.parse(acces);
